347c1ddac9c62652972d6554fdf4891e8c99189c
DeadZoo <chrisjohnacc@gmail.com>
2021-09-02 16:31:35 +0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('GoodNight Underworld'));
+app.get('/', (req, res) => res.send('GoodNight SUNderworld'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
