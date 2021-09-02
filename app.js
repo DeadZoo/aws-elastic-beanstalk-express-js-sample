12c6b3baa3527e638bc874b540546b4740061893
DeadZoo <chrisjohnacc@gmail.com>
2021-09-02 16:44:16 +0300

    New Message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('GoodNight SUNderworld'));
+app.get('/', (req, res) => res.send('This is horrendously awful'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
